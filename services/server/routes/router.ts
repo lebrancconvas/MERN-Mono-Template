db5a17e795c1f9dfc294103f3cd05a06207b3e72
lebrancconvas <lebrancconvas@gmail.com>
2022-07-25 12:49:01 +0700

    ✨ feat: update.

---
+++ services/server/routes/router.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response, Router } from 'express';
+
+const router: Router = express.Router();
+
+router.use((req: Request, res: Response, next: Function) => {
+	console.log(`[${req}]`);
+})
+
+router.get('/', (req: Request, res: Response) => {
+	res.send("Hello Server.");
+});
+
+export default router;
