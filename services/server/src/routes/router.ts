fb9d9960c7021ccff29240bb1443eed6f785de7c
lebrancconvas <lebrancconvas@gmail.com>
2022-07-25 12:52:08 +0700

    🔨 feat: update server.

--- services/server/src/routes/router.ts
+++ services/server/src/routes/router.ts
@@ -3,7 +3,8 @@ import express, { Request, Response, Router } from 'express';
 const router: Router = express.Router();
 
 router.use((req: Request, res: Response, next: Function) => {
-	console.log(`[${req}]`);
+	console.log(`[${req.method}] ${req.url}`);
+	next();
 })
 
 router.get('/', (req: Request, res: Response) => {
